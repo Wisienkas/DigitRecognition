c0afd996e6c2ff9794c5eba8ea4d14a1a44b5861
Nikolaj Schaldemose Reibke <nikolajsr12@gmail.com>
2015-06-05 14:38:54 +0200

    ll

--- code/run_randomForest.r
+++ code/run_randomForest.r
@@ -6,6 +6,7 @@ pacman::p_load(randomForest)
 run_randomForest.easy <- function(df) {
   folds <- pre_transform.folds(df = df)
   
+  results <- list()
   for(i in 1:length(folds)) {
     test <- list()
     train <- list()
@@ -17,6 +18,8 @@ run_randomForest.easy <- function(df) {
         train$cl <- factor(c(as.character(train$cl), as.character(folds[[j]]$df)))
       }
     }
+    
+    results <- rfcv(trainx = train$df, trainy = train$cl, cv.fold = 10, )
   }
 }
 
