705e393f118a2e2bb4d895c04e7b33c1187837e4
Nikolaj Schaldemose Reibke <nikolajsr12@gmail.com>
2015-06-06 16:42:27 +0200

    we are DRUNK!

--- code/alg_randomForest.r
+++ code/alg_randomForest.r
@@ -12,23 +12,38 @@ alg_randomForest.easy <- function(df, ntree, digits_per_person = 4000) {
 }
 
 alg_randomForest.run_folds <- function(folds, ntree) {
-  results <- list()
-  for(i in 1:length(folds)) {
-    test <- list()
-    train <- list()
-    for(j in 1:length(folds)) {
-      if(i == j) {
-        test <- folds[[j]]
+  # time, ntree, avg correctness, digit correctness
+  results <- matrix(nrow = 0, ncol = 13)
+  colnames(results) <- c("ntree", "time", "avg correctness", 0:9)
+  for(trees in ntree) {
+    digitSuccess <- c()
+    start_time <- Sys.time()
+    for(i in 1:length(folds)) {
+      test <- list()
+      train <- list()
+      for(j in 1:length(folds)) {
+        if(i == j) {
+          test <- folds[[j]]
+        } else {
+          train$df <- rbind(train$df, folds[[j]]$df)
+          train$cl <- factor(c(as.character(train$cl), as.character(folds[[j]]$cl)), levels = 1:10)
+        }
+      }
+      rf <- randomForest(x = train$df, y = train$cl, ntree = trees, xtest = test$df, ytest = test$cl)
+      pred <- c()
+      for(level in levels(test$cl)) {
+        pred[level] <- sum(rf$test$predicted == test$cl & test$cl == level) / sum(test$cl == level)
+      }
+      if(i == 1) {
+        digitSuccess <- pred
       } else {
-        train$df <- rbind(train$df, folds[[j]]$df)
-        train$cl <- factor(c(as.character(train$cl), as.character(folds[[j]]$cl)), levels = 1:10)
+        digitSuccess <- digitSuccess + pred
       }
     }
-    rf <- randomForest(x = train$df, y = train$cl, ntree = ntree, xtest = test$df, ytest = test$cl)
-    results[[i]] <- list()
-    for(level in levels(test$cl)) {
-      results[[i]][[level]] <- sum(rf$test$predicted == test$cl & test$cl == level) / sum(test$cl == level)
-    }
+    end_time <- Sys.time() - start_time
+    digitSuccess <- digitSuccess / length(folds)
+    avgCorrect <- mean(digitSuccess)
+    results <- rbind(results, c("trees" = trees, "time" = end_time, "avg correctness" = avgCorrect, digitSuccess))
   }
   return(results)
 }
--- code/run_randomForest.r
+++ code/run_randomForest.r
@@ -1,49 +1,60 @@
 #code needed to run random forest
 
 # Load required files
-source("img_loader.R")
-source("pre_PCA.R")
+source("img_loader.r")
+source("pre_PCA.r")
 source("pre_transform.R")
 source("pre_KMeans.R")
-source("alg_randomForest.R")
+source("alg_randomForest.r")
 
 
 # Load all Data
 data <- img_loader.allPersons()
-data1 <- data
-for(i in 1:length(data1)) {
-  while(ncol(data1[[i]]) != 380) {
-    data1[[i]] <- cbind(data1[[i]], rep(1.0, 400))
+data <- img_loader.singlePerson(group = "group1", member = "member1")
+
+result_data <- NULL
+
+clusters <- c(10, 20, 40, 80);
+pca <- c(0.9, 0.95, 0.99);
+
+ntree <- c()
+
+km_list <- list()
+
+for(i in 1:length(clusters)) {
+  start_time <- Sys.time();
+  cluster <- clusters[i]
+  km_data <- pre.KMeans(imageData = data, clusters_per_digit = cluster)
+  end_time <- Sys.time() - start_time
+  km_list[[i]] <- list("km_clusters" = cluster, 
+                       "km_data" = pre_transform.transform(imageData = km_data), 
+                       "km_time" = end_time)
+}
+
+# Running with only kmeans
+for(item in km_list) {
+  start_time <- Sys.time();
+  rf <- alg_randomForest.easy(df = item$km_data, ntree = ntree, digits_per_person = item$clusters * 10)
+  rf_km <- cbind(rf, "mode" = paste("kmeans", item$km_clusters, sep = "_"))
+  if(is.null(result_data)) {
+    result_data <- rf_km
+  } else {
+    result_data <- rbind(result_data, rf_km)
   }
 }
 
-clusters <- c(10, 20, 40, 80);
-km_rf_result <- list()
-km_pca_rf_result <- list()
-km_times <- list()
-km_pca_times <- list()
-
-# Running with only kmeans
-for(i in 1:length(clusters)) {
-  start_time <- Sys.time();
-  print("Starting")
-  km_data <- pre.KMeans(imageData = data1, clusters_per_digit = clusters[i])
-  
-  km_data_df <- pre_transform.transform(imageData = km_data)
-  print("Doing random forest")
-  km_rf_result[[i]] <- alg_randomForest.easy(df = km_data_df, ntree = sqrt(nrow(km_data_df)), digits_per_person = clusters[i] * 10)
-  km_times[[i]] <- Sys.time() - start_time 
+# Running with only PCA
+for(s in smoothing) {
+  for(p in pca) {
+    start_time <- Sys.time();
+    data.pca <- pre.PCA(imageData = data, coverage = p)
+    rf <- alg_randomForest.easy(df = data.pca, ntree = ntree, digits_per_person = 4000)
+    rf_km <- cbind(rf, "mode" = paste("pca", p, "blur", s, sep = "_"))
+    if(is.null(result_data)) {
+      result_data <- rf_km
+    } else {
+      result_data <- rbind(result_data, rf_km)
+    }
+  }
 }
-
-# PCA(95 %) and Kmeans
-for(i in 1:length(clusters)) {
-  start_time <- Sys.time();
-  km_data <- pre.KMeans(imageData = data, clusters_per_digit = clusters[i])
-  
-  km_pca_data_df <- pre.PCA(imageData = km_data, coverage = 0.95)
-  
-  km_pca_rf_result[[i]] <- alg_randomForest.easy(df = km_pca_data_df, ntree = sqrt(nrow(km_data_df)), digits_per_person = clusters[i] * 10)
-  km_times[[i]] <- Sys.time() - start_time 
-}
-
 
