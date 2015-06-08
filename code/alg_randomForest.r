06a3698df8bd648909a155d84db7e9e293a1cf0f
Nikolaj Reibke <nikolajsr12@gmail.com>
2015-06-08 14:33:32 +0200

    Adding images

--- code/alg_randomForest.r
+++ code/alg_randomForest.r
@@ -5,13 +5,14 @@
 if (!require("pacman")) install.packages("pacman")
 pacman::p_load(randomForest)
 
-alg_randomForest.easy <- function(df, ntree, digits_per_person = 4000) {
+alg_randomForest.easy <- function(df, ntree, test, digits_per_person = 4000, digits_per_person_test = 4000) {
   folds <- pre_transform.folds(df = df, f = 10, digits_per_person = digits_per_person)
+  folds.test <- pre_transform.folds(df = test, f = 10, digits_per_person = digits_per_person)
   
-  return(alg_randomForest.run_folds(folds, ntree))
+  return(alg_randomForest.run_folds(folds, folds.test, ntree))
 }
 
-alg_randomForest.run_folds <- function(folds, ntree) {
+alg_randomForest.run_folds <- function(folds, folds.test, ntree) {
   # time, ntree, avg correctness, digit correctness
   results <- matrix(nrow = 0, ncol = 13)
   colnames(results) <- c("ntree", "time", "avg correctness", 0:9)
@@ -23,7 +24,7 @@ alg_randomForest.run_folds <- function(folds, ntree) {
       train <- list()
       for(j in 1:length(folds)) {
         if(i == j) {
-          test <- folds[[j]]
+          test <- folds.test[[j]]
         } else {
           train$df <- rbind(train$df, folds[[j]]$df)
           train$cl <- factor(c(as.character(train$cl), as.character(folds[[j]]$cl)), levels = 1:10)
